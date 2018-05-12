64767129138d432c1c5d20641cf6c4e0c9499bf4
gasparia405 <gasparia405@gmail.com>
2018-05-12 13:03:32 -0400

    Initial commit
    Basic HTML Layout, basic styling, some JS variables set

---
+++ quizInteraction.js
@@ -0,0 +1,19 @@
+// create questions and answers
+
+const questions = {
+    question1: `<p>What was the first hit single from Country music singer Brett Young?</p>`,
+    question2: `<p>In which song does Kenny Chesney team up with Uncle Kracker?</p>`,
+    question3: `<p>Which Country artist was formerly the lead singer of Hooty and the Blowfish?</p>`,
+    question4: `<p>Which female duo teamed up to paint the town red in "Somethin' Bad"?</p>`
+}
+
+const answers = {
+    answer1: ,
+    answer2: ,
+    answer3: ,
+    answer4: 'li'
+}
+
+// change answers
+
+// display answer given and correct answer
